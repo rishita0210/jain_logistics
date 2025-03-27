c9024afd4be2cc707d67f85fc88e600589902254
rishita0210 <rishitadixit02@gmail.com>
2025-03-27 14:10:43 +0530

    hero modified

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -18,17 +18,15 @@ const Hero = () => {
       <div className="relative z-10 flex flex-col md:flex-row items-center justify-between max-w-7xl mx-auto px-6">
         {/* Left Side Text */}
         <div className="md:w-1/2 text-white">
-          <h1 className="text-4xl md:text-6xl font-bold text-[#E2E2B6] mt-10 mb-4">Jain Logisitcs</h1>
-          <h1 className="text-4xl md:text-6xl font-bold mb-6">
-            India's Leading <br/> Logistics Partner
+          <h1 className="text-4xl md:text-5xl font-bold text-[#E2E2B6] mt-10 mb-4">Jain Logisitcs</h1>
+          <h1 className="text-4xl md:text-5xl font-bold mb-6">
+            India's Leading  Logistics Partner
           </h1>
-          {/* <button className="px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
-            Get Started
-          </button> */}
+          
         </div>
 
         {/* Right Side Overlapping Image */}
-        <div className="md:w-1/2 relative">
+        {/* <div className="md:w-1/2 relative">
           <Image
             src="/truck.png"  
             alt="Overlapping Visual"
@@ -36,7 +34,7 @@ const Hero = () => {
             height={500}
             className="relative md:ml-10 md:mt-70 drop-shadow-lg"
           />
-        </div>
+        </div> */}
       </div>
     </section>
   );
